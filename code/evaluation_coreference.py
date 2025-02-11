e90e5c61d4a77ba37159f864f35a498f26761b07
FanZT6 <122083690+FanZT6@users.noreply.github.com>
2025-02-12 00:32:20 +0800

    Update evaluation_coreference.py

--- code/evaluation_coreference.py
+++ code/evaluation_coreference.py
@@ -251,7 +251,7 @@ for inputs in tqdm(data):
 print(f"\nCompleted, pelase check {save_name}")
 
 '''
-export OPENAI_API_KEY=sk-proj-xiYnPJXGZl4KuTYNlqWiT3BlbkFJiH91G4NkkKiehAWnAMiw
+export OPENAI_API_KEY=[Your_OPENAI_API_KEY]
 
 python coreference/evaluation.py \
 --model "gpt4" --prompt prompt_standard.txt \
